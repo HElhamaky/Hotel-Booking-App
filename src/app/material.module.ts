ed6a156abad637c33851a6d1156b36ebee394653
hassan elhamaky <hassan.elhamaky@yahoo.com>
2020-01-10 23:15:11 +0200

    Material Module has been created and configured

---
+++ src/app/material.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { MatSliderModule } from '@angular/material/slider';
+
+
+@NgModule({
+    imports:[],
+    exports:[MatSliderModule]
+})
+
+export class MaterialModule {
+
+}
