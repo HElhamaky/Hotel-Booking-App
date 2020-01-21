f5be29b0a4727afd395ab635d5b92c4bc2c3d740
hassan elhamaky <hassan.elhamaky@yahoo.com>
2020-01-21 23:24:57 +0200

    working on navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -23,6 +23,10 @@ import { AuthComponent } from './auth/auth.component';
 import { SigninComponent } from './auth/signin/signin.component';
 import { SignupComponent } from './auth/signup/signup.component';
 import { AppRoutingModule } from './app-routing.module';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './navigation/header/header.component';
+import { SidenavListComponent } from './navigation/sidenav-list/sidenav-list.component';
+import { NavtabsComponent } from './navigation/navtabs/navtabs.component';
 
 @NgModule({
   declarations: [
@@ -44,7 +48,11 @@ import { AppRoutingModule } from './app-routing.module';
     CelebrationsComponent,
     AuthComponent,
     SigninComponent,
-    SignupComponent
+    SignupComponent,
+    HomeComponent,
+    HeaderComponent,
+    SidenavListComponent,
+    NavtabsComponent
   ],
   imports: [
     BrowserModule,
--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -2,6 +2,13 @@ import { NgModule } from '@angular/core';
 import { MatSliderModule } from '@angular/material/slider';
 import { MatBadgeModule } from '@angular/material/badge';
 import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatListModule} from '@angular/material/list';
+
+
+
 
 
 @NgModule({
@@ -9,7 +16,12 @@ import {MatButtonModule} from '@angular/material/button';
     exports:[
         MatSliderModule,
         MatBadgeModule,
-        MatButtonModule
+        MatButtonModule,
+        MatToolbarModule,
+        MatIconModule,
+        MatSidenavModule,
+        MatListModule
+
     ]
 })
 
